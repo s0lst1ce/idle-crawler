9e01a1db03a44418f4fb298e9ec30d82cfd05c39
s0lst1ce <thithib.cohergne@gmail.com>
2020-06-02 15:25:32 +0200

    add PosGenerator Iterator

--- src/server/src/lib.rs
+++ src/server/src/lib.rs
@@ -5,7 +5,7 @@ use std::fs::File;
 use std::hash::Hash;
 use std::io;
 use std::path::PathBuf;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 //tile position
 #[derive(Debug, Serialize, Deserialize, PartialEq, Eq, Hash)]
@@ -71,9 +71,74 @@ struct Player {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Game {
+    //we need to make sure they stay ordered to enable correct behavior of PosGenerator
     world: HashMap<Position, Tile>,
     //HashMap<"username", Player>
     players: HashMap<String, Player>,
+}
+
+const VERTICE_COUPLE: [Position; 2] = [Position { x: 1, y: 0 }, Position { x: 0, y: -1 }];
+
+#[derive(Debug)]
+struct PosGenerator {
+    step: u32,
+    last_pos: Position,
+    tiles: Vec<Position>,
+    vertices: Vec<Position>,
+}
+
+impl PosGenerator {
+    pub fn new(step: u32) -> PosGenerator {
+        PosGenerator {
+            step: step,
+            tiles: Vec::new(),
+            vertices: Vec::new(),
+            last_pos: Position { x: 0, y: 0 },
+        }
+    }
+    fn total_pos(&self) -> u32 {
+        let nbr = 1;
+        for i in 0..self.step {
+            nbr += 2 * i;
+        }
+        nbr
+    }
+
+    fn next_vertices(&mut self) -> () {
+        let mut sign: i32 = 1;
+        for _ in 0..2 {
+            for i in 0..2 {
+                self.vertices.push(Position {
+                    x: VERTICE_COUPLE[i].x * sign * self.step as i32,
+                    y: VERTICE_COUPLE[i].y * sign * self.step as i32,
+                });
+            }
+            sign *= -1;
+        }
+    }
+
+    fn next_tiles(&mut self) -> () {
+        if self.vertices.len() == 0 {
+            self.next_vertices();
+        }
+        let Position { x, y } = self.vertices.remove(0);
+        for i in 0..(x | y) {
+            self.tiles.push(Position {
+                x: (self.last_pos.x | self.last_pos.x + i),
+                y: (self.last_pos.y | self.last_pos.y + i),
+            })
+        }
+    }
+}
+
+impl Iterator for PosGenerator {
+    type Item = Position;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.tiles.len() == 0 {
+            self.next_tiles();
+        }
+        Some(self.tiles.remove(0))
+    }
 }
 
 impl Game {
@@ -91,5 +156,24 @@ impl Game {
         )?;
         serde_json::to_writer(file, self)?;
         Ok(())
+    }
+
+    pub fn load(&self, path: PathBuf) -> Result<Game, io::Error> {
+        let file = File::open(path)?;
+        let game: Game = serde_json::from_reader(file)?;
+        Ok(game)
+    }
+
+    pub fn new(nbr: u32) -> Game {
+        let world = HashMap::new();
+        for _ in 0..nbr {
+            tiles.push(Tile::new());
+        }
+    }
+}
+
+impl Tile {
+    fn new() -> Tile {
+        unimplemented!()
     }
 }
